ad0deb8135b28bd53895ce83a6cb1d24d40a9cac
ThePotatoArchivist <josiahfu@gmail.com>
2025-10-10 16:07:35 -0700

    Make PWA for offline usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const nonHashFiles = [
     'woff',
@@ -15,6 +16,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    VitePWA(),
   ],
   base: '/math-notes',
   build: {
